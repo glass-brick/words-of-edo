4e0d164f09e9f4d6d5f9d9ffdd7d237100eb52de
Julián Absatz <julianabsatz@gmail.com>
2020-08-22 22:35:35 +0200

    progress

--- src/Battle.jsx
+++ src/Battle.jsx
@@ -1,40 +1,39 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import "./Battle.scss";
 import data from "./data.js";
 import useBattleState from "./useBattleState";
+import Dropdown from "./Battle/Dropdown";
 
 function Room({
-  wordToTell,
   onComplete = () => {},
   enemyWord,
   onEnemyComplete = () => {},
+  onKeyStroke,
+  monster,
 }) {
   const [input, setInput] = useState("");
 
-  const leftoverWord = wordToTell.substring(input.length);
-
   useEffect(() => {
     const handler = (e) => {
-      if (
-        leftoverWord &&
-        e.key.toLowerCase() === leftoverWord[0].toLowerCase()
-      ) {
-        setInput((input) => `${input}${leftoverWord[0]}`);
-        if (leftoverWord.length === 1) {
-          setInput("");
-          onComplete();
-        }
+      console.log(e);
+      if (e.code.startsWith("Key") || e.code === "Space") {
+        onKeyStroke();
+        setInput((input) => `${input}${e.key}`);
+      }
+      if (e.code === "Enter") {
+        onComplete(input);
+        setInput("");
       }
     };
 
     document.addEventListener("keydown", handler);
 
     return () => document.removeEventListener("keydown", handler);
-  }, [leftoverWord, onComplete]);
+  }, [input, onComplete, onKeyStroke]);
 
   const [enemyInput, setEnemyInput] = useState("");
 
-  const leftoverEnemyWord = enemyWord.substring(enemyInput.length);
+  const leftoverEnemyWord = enemyWord && enemyWord.substring(enemyInput.length);
 
   useEffect(() => {
     let id;
@@ -45,22 +44,19 @@ function Room({
           setEnemyInput("");
           onEnemyComplete();
         }
-      }, 500);
+      }, monster.msperkeystroke);
     }
     return () => {
       if (id) clearTimeout(id);
     };
-  }, [leftoverEnemyWord, onEnemyComplete]);
+  }, [monster, leftoverEnemyWord, onEnemyComplete]);
 
   return (
     <div className="room">
       <div className="room__player">
-        {wordToTell && (
-          <div className="room__speech">
-            <span className="room__speech__said">{input}</span>
-            <span className="room__speech__leftover">{leftoverWord}</span>
-          </div>
-        )}
+        <div className="room__speech">
+          <span className="room__speech__said">{input}</span>
+        </div>
       </div>
       <div className="room__enemy">
         {enemyWord && (
@@ -69,34 +65,46 @@ function Room({
             <span className="room__speech__leftover">{leftoverEnemyWord}</span>
           </div>
         )}
+        <img
+          alt={monster.name}
+          src={monster.sprite}
+          style={{
+            height: "300px",
+          }}
+        />
       </div>
     </div>
   );
 }
 
-export default function Battle(props) {
-  const state = useBattleState({ monster: data.monsters[0] });
+function Header(props) {
+  return (
+    <header className="top-menu">
+      <Dropdown title="Items" />
+      <Dropdown title="Spells" options={Object.values(data.spells)} />
+    </header>
+  );
+}
+
+export default function Battle({ monster = data.monsters.monster_prototype }) {
+  const state = useBattleState({ monster });
 
   return (
     <div className="battle">
-      <header className="top-menu">
-        <button className="top-menu__button">Items</button>
-        <button className="top-menu__button">Spells</button>
-      </header>
+      <Header />
 
       <Room
-        wordToTell="Azrath mithrion sinthos"
-        enemyWord="Awante bokita"
-        onComplete={() => {}}
-        onEnemyComplete={() => {}}
+        enemyWord={state.enemyWord}
+        onComplete={state.onCompleteWord}
+        onEnemyComplete={state.onCompleteEnemyWord}
+        onKeyStroke={state.onKeyStroke}
+        monster={monster}
       />
 
       <div className="bottom-menu">
-        <div className="bottom-menu__hp">HP: 9/10</div>
-        <div className="bottom-menu__objective-data">House: 100%</div>
-        <div className="bottom-menu__objective">
-          Objective: Don't burn the house
-        </div>
+        <div className="bottom-menu__hp">HP: {state.hp}/1000</div>
+        <div className="bottom-menu__objective-data">TBD</div>
+        <div className="bottom-menu__objective">TBD</div>
         <ul className="bottom-menu__log">
           {state.log.map((line, i) => (
             <li key={i}>{line}</li>
--- src/data.js
+++ src/data.js
@@ -1,23 +1,29 @@
-const data = {
-  monsters: [
-    {
-      name: "monster_prototype",
-      speed: 0.026,
-      hp: 400,
-      attackchance: 0.15,
-      msperkeystroke: 750,
-      spells: "roku",
-    },
-  ],
-  spells: [
-    {
-      name: "roku",
-      damage: 15,
-      special: "",
-      condition: "fire",
-      descrition: "Small yet damaging sparks emanate from the monk",
-    },
-  ],
+import monsterPrototype from "./assets/monster_prototype.png";
+
+const spells = {
+  roku: {
+    name: "roku",
+    displayName: "Roku",
+    damage: 15,
+    special: "",
+    condition: "fire",
+    descrition: "Small yet damaging sparks emanate from the monk",
+  },
 };
 
-export default data;
+const monsters = {
+  monster_prototype: {
+    name: "monster_prototype",
+    speed: 0.026,
+    hp: 400,
+    attackchance: 0.15,
+    msperkeystroke: 750,
+    spells: [spells.roku],
+    sprite: monsterPrototype,
+  },
+};
+
+export default {
+  monsters,
+  spells,
+};
