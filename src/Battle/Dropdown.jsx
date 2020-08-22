dc8ddca202833b8c8722608d67136c2be2d8ce0f
Julián Absatz <julianabsatz@gmail.com>
2020-08-22 23:08:33 +0200

    added spell description submenu

--- src/Battle/Dropdown.jsx
+++ src/Battle/Dropdown.jsx
@@ -4,12 +4,19 @@ import "./Dropdown.scss";
 
 export default function Dropdown({ title, options = [] }) {
   const [open, setOpen] = useState(false);
+  const [subMenu, setSubMenu] = useState(null);
   const disabled = options.length === 0;
 
   const buttonRef = useRef(null);
   const boundingRect = buttonRef.current
     ? buttonRef.current.getBoundingClientRect()
     : {};
+
+  const subMenuItem = document.getElementById(subMenu);
+  const subMenuBoundingRect = subMenuItem
+    ? subMenuItem.getBoundingClientRect()
+    : {};
+  const subMenuData = options.find((option) => option.name === subMenu);
 
   return (
     <button
@@ -22,21 +29,40 @@ export default function Dropdown({ title, options = [] }) {
       <span>{title}</span>
       {open &&
         createPortal(
-          <div
-            className="dropdown-portal"
-            style={{
-              top: boundingRect.bottom + window.scrollY,
-              left: boundingRect.left + window.scrollX,
-            }}
-          >
-            <ul className="dropdown-portal__list">
-              {options.map((option) => (
-                <li key={option.name} className="dropdown-portal__list__item">
-                  {option.displayName}
-                </li>
-              ))}
-            </ul>
-          </div>,
+          <>
+            <div
+              className="dropdown-portal"
+              style={{
+                top: boundingRect.bottom + window.scrollY,
+                left: boundingRect.left + window.scrollX,
+              }}
+            >
+              <ul className="dropdown-portal__list">
+                {options.map((option) => (
+                  <li
+                    key={option.name}
+                    id={option.name}
+                    className="dropdown-portal__list__item"
+                    onMouseOver={() => setSubMenu(option.name)}
+                  >
+                    {option.displayName}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {subMenuItem && (
+              <div
+                className="dropdown-portal__submenu"
+                style={{
+                  position: "absolute",
+                  top: subMenuBoundingRect.top + window.scrollY,
+                  left: subMenuBoundingRect.right + window.scrollX + 21,
+                }}
+              >
+                {subMenuData.description}
+              </div>
+            )}
+          </>,
           document.getElementById("portal-root")
         )}
     </button>
