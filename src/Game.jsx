9300b1ddfabe603cd67a6737c86f1249ea2a7b9d
Julián Absatz <julianabsatz@gmail.com>
2020-08-24 01:25:58 +0200

    added game intro

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Battle from "./Battle";
-import Intro from "./Intro/Intro";
+import Menu from "./Menu/Menu";
 import data from "./data";
+import Intro from "./Intro/Intro";
 
-function Game() {
-  const [monk, setMonk] = useState(data.monk);
-  const [currentMission, setCurrentMission] = useState(null);
+function useTransitionState(initialState) {
+  const [state, setState] = useState(initialState);
   const [transition, setTransition] = useState(false);
 
   useEffect(() => {
@@ -20,48 +20,86 @@ function Game() {
     };
   }, [transition]);
 
+  const transitionTo = useCallback(
+    (newState) =>
+      new Promise((resolve) => {
+        if (!transition) setTransition(true);
+        setTimeout(() => {
+          setState(newState);
+          resolve();
+        }, 500);
+      }),
+    [transition]
+  );
+
+  return [state, transitionTo, transition];
+}
+
+function Game() {
+  const [monk, setMonk] = useState(data.monk);
+  const [gameScreen, setGameScreen, transition] = useTransitionState({
+    // type: localStorage.getItem("introComplete") === "true" ? "menu" : "intro", <- play intro only once
+    type: "intro",
+  });
+
+  let currentScreen;
+
+  switch (gameScreen.type) {
+    case "intro":
+      currentScreen = (
+        <Intro
+          onIntroEnd={() => {
+            localStorage.setItem("introComplete", "true");
+            setGameScreen({ type: "menu" });
+          }}
+        />
+      );
+      break;
+    case "mission":
+      currentScreen = (
+        <Battle
+          monk={monk}
+          mission={gameScreen.mission}
+          onMissionEnd={(results) => {
+            if (results.rewards) {
+              if (results.rewards.spells) {
+                // here we check if the monk already has these spells
+                let newSpells = [];
+                results.rewards.spells.forEach((spell) => {
+                  let monkHasIt = false;
+                  monk.spells.forEach((monkSpell) => {
+                    if (monkSpell.name === spell.name) {
+                      monkHasIt = true;
+                      return;
+                    }
+                  });
+                  if (!monkHasIt) newSpells.push(spell);
+                });
+                setMonk({ ...monk, spells: [...monk.spells, ...newSpells] });
+              }
+            }
+            setGameScreen({ type: "menu" });
+          }}
+        />
+      );
+      break;
+    case "menu":
+    default:
+      currentScreen = (
+        <Menu
+          onMissionStart={(mission) => {
+            setGameScreen({ type: "mission", mission });
+          }}
+        />
+      );
+      break;
+  }
+
   return (
     <>
       <div className="game-app">
         {transition && <div className="transition" />}
-        {currentMission ? (
-          <Battle
-            monk={monk}
-            mission={currentMission}
-            onMissionEnd={(results) => {
-              if (results.rewards) {
-                if (results.rewards.spells) {
-                  // here we check if the monk already has these spells
-                  let newSpells = [];
-                  results.rewards.spells.forEach((spell) => {
-                    let monkHasIt = false;
-                    monk.spells.forEach((monkSpell) => {
-                      if (monkSpell.name === spell.name) {
-                        monkHasIt = true;
-                        return;
-                      }
-                    });
-                    if (!monkHasIt) newSpells.push(spell);
-                  });
-                  setMonk({ ...monk, spells: [...monk.spells, ...newSpells] });
-                }
-              }
-              if (!transition) setTransition(true);
-              setTimeout(() => {
-                setCurrentMission(null);
-              }, 500);
-            }}
-          />
-        ) : (
-          <Intro
-            onMissionStart={(mission) => {
-              if (!transition) setTransition(true);
-              setTimeout(() => {
-                setCurrentMission(mission);
-              }, 500);
-            }}
-          />
-        )}
+        {currentScreen}
       </div>
       <div id="portal-root"></div>
     </>
--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import edo from "../assets/edo.png";
 import "./Intro.scss";
 import data from "../data";
+import arrowLeft from "../assets/arrow_left.png";
 
-export default function Intro({ onMissionStart = () => {} }) {
+const introTexts = [
+  "I don't yet know the words of Edo",
+  "The only words I know here are Kanae, Roku and Mamoku",
+  "With Roku I can burn them",
+  "With Kanae I can hit them",
+  "With Mamoku I can protect myself from them at the last minute",
+  "The demons can use them too",
+  "Their spells get stronger the closer they get",
+  "But they can only move when they hear me chanting",
+  "It is my duty to hunt these monsters and to take care of Edo's people",
+];
+
+export default function Intro({ onIntroEnd = () => {} }) {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
   return (
     <div className="intro">
-      <img src={edo} alt="Edo" />
-      <div className="intro__menu">
-        <h1 className="intro__menu__title">Words of Edo</h1>
-        <div className="intro__menu__list">
-          {data.missions.map((mission) => (
-            <div
-              className="intro__menu__list__item"
-              onClick={() => onMissionStart(mission)}
-              key={mission.title}
-            >
-              <h2 className="intro__menu__list__item__title">
-                {mission.title}
-              </h2>
-              <p className="intro__menu__list__item__description">
-                {mission.description}
-              </p>
-              <img
-                className="intro__menu__list__item__img"
-                alt="Monster"
-                src={mission.image}
-              />
-            </div>
-          ))}
+      <div className="intro__text-box">
+        <p className="intro__text-box__text">{introTexts[currentTextIndex]}</p>
+        <div className="intro__text-box__arrow">
+          <img
+            onClick={() => {
+              if (currentTextIndex < introTexts.length - 1) {
+                setCurrentTextIndex(currentTextIndex + 1);
+              } else {
+                onIntroEnd();
+              }
+            }}
+            src={arrowLeft}
+            alt="Right"
+            style={{
+              transform: "scaleX(-1)",
+              gridColumn: 3,
+              float: "right",
+              cursor: "pointer",
+            }}
+          />
         </div>
       </div>
     </div>
