ea1ee73add9e5bfd551c9c86ba5a2c8f372f1354
Julián Absatz <julianabsatz@gmail.com>
2020-08-23 23:07:35 +0200

    added navigation to spellbook

--- src/Battle/SpellBook.jsx
+++ src/Battle/SpellBook.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import arrowLeft from "../assets/arrow_left.png";
 import cx from "classnames";
 import "./SpellBook.scss";
 
@@ -19,7 +20,7 @@ export default function SpellBook({ spells, show, onClose }) {
   // a "page" represents everything that can be seen at the same time. it might look like two pages, but they're counted as one
   // they're also 0-indexed (first page is 0)
   const [currentPage, setCurrentPage] = useState(0);
-  const pages = Math.floor(spells.length / (SPELLS_PER_FACE * 2));
+  const faces = Math.ceil(spells.length / SPELLS_PER_FACE);
 
   const face1Spells = spells.slice(
     currentPage * (SPELLS_PER_FACE * 2),
@@ -30,10 +31,14 @@ export default function SpellBook({ spells, show, onClose }) {
     (currentPage + 1) * (SPELLS_PER_FACE * 2)
   );
 
+  const canGoLeft = currentPage > 0;
+  const canGoRight = (currentPage + 1) * SPELLS_PER_FACE * 2 < spells.length;
+
   const ref = useRef(null);
 
   useEffect(() => {
     const handler = (e) => {
+      console.log(e.target);
       if (!ref.current.contains(e.target) && show) {
         onClose();
       }
@@ -56,11 +61,48 @@ export default function SpellBook({ spells, show, onClose }) {
         {face1Spells.map((spell, i) => (
           <Spell key={i} spell={spell} />
         ))}
+        <div className="spellbook__face__bottom">
+          <img
+            onClick={() => setCurrentPage(currentPage - 1)}
+            src={arrowLeft}
+            alt="Left"
+            style={{
+              display: canGoLeft ? "block" : "none",
+              gridColumn: 1,
+              justifySelf: "flex-start",
+            }}
+          />
+          <span style={{ gridColumn: 2, justifySelf: "center" }}>
+            {currentPage * 2 + 1} of {faces}
+          </span>
+        </div>
       </div>
       <div className="spellbook__face">
         {face2Spells.map((spell, i) => (
           <Spell key={i} spell={spell} />
         ))}
+        <div className="spellbook__face__bottom">
+          <span
+            style={{
+              gridColumn: 2,
+              display: face2Spells.length > 0 ? "block" : "none",
+              justifySelf: "center",
+            }}
+          >
+            {currentPage * 2 + 2} of {faces}
+          </span>
+          <img
+            onClick={() => setCurrentPage(currentPage + 1)}
+            src={arrowLeft}
+            alt="Right"
+            style={{
+              transform: "scaleX(-1)",
+              gridColumn: 3,
+              display: canGoRight ? "block" : "none",
+              justifySelf: "flex-end",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
