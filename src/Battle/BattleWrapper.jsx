9c7ef292e278fc4ae9f689738ef99236cace7136
Julián Absatz <julianabsatz@gmail.com>
2020-08-24 05:11:24 +0200

    win/lose screens

---
+++ src/Battle/BattleWrapper.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import Battle from "../Battle";
+import { useTransitionState } from "../Game";
+import "./BattleWrapper.scss";
+
+export default function BattleWrapper({ onMissionEnd, ...props }) {
+  const [battleState, setBattleState, transition] = useTransitionState("intro");
+  const [savedMissionResult, setSavedMissionResult] = useState(null);
+
+  useEffect(() => {
+    let id;
+    if (battleState === "intro") {
+      id = setTimeout(() => {
+        setBattleState("battle");
+      }, 1000);
+    }
+    return () => clearTimeout(id);
+  }, [battleState, setBattleState]);
+
+  const handleMissionEnd = (missionResult) => {
+    setBattleState(missionResult.monkDead ? "lose" : "win");
+    setSavedMissionResult(missionResult);
+  };
+
+  let element;
+  switch (battleState) {
+    case "intro":
+      element = (
+        <div className="battle--intro">
+          <div className="battle__title">
+            Battle against {props.mission.monster.displayName}
+          </div>
+        </div>
+      );
+      break;
+    case "win":
+      element = (
+        <div className="battle--win-screen">
+          <div className="battle__title">
+            You defeated {props.mission.monster.displayName}!
+          </div>
+          <div className="battle__subtitle">
+            {savedMissionResult.missionObjectivePassed
+              ? "You also passed the objective!"
+              : "But you didn't manage to accomplish the objective..."}
+          </div>
+          <button
+            className="button"
+            onClick={() => onMissionEnd(savedMissionResult)}
+          >
+            Continue
+          </button>
+        </div>
+      );
+      break;
+    case "lose":
+      element = (
+        <div className="battle--lose-screen">
+          <div className="battle__title">You lost!</div>
+          <button
+            className="button"
+            onClick={async () => {
+              await setBattleState("intro");
+              setSavedMissionResult(null);
+            }}
+          >
+            Try again
+          </button>
+          <button
+            className="button"
+            onClick={() => onMissionEnd(savedMissionResult)}
+          >
+            Return to mission select
+          </button>
+        </div>
+      );
+      break;
+    case "battle":
+    default:
+      element = <Battle {...props} onMissionEnd={handleMissionEnd} />;
+      break;
+  }
+
+  return (
+    <>
+      {transition && <div className="transition" />}
+      {element}
+    </>
+  );
+}
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useCallback } from "react";
-import Battle from "./Battle";
 import Menu from "./Menu/Menu";
 import data from "./data";
 import Intro from "./Intro/Intro";
+import BattleWrapper from "./Battle/BattleWrapper";
 
-function useTransitionState(initialState) {
+export function useTransitionState(initialState) {
   const [state, setState] = useState(initialState);
   const [transition, setTransition] = useState(false);
 
@@ -57,7 +57,7 @@ function Game() {
       break;
     case "mission":
       currentScreen = (
-        <Battle
+        <BattleWrapper
           monk={monk}
           mission={gameScreen.mission}
           onMissionEnd={(results) => {
@@ -65,58 +65,65 @@ function Game() {
             let newSpells = [];
             let finalItems = [...monk.items];
             let finalSpells = [...monk.spells];
-            
-            if(results.usedItems){
+
+            if (results.usedItems) {
               let oldItems = monk.items;
               let usedItems = [];
               let used;
               let objectUsed;
               let indexUsed;
-              results.usedItems.forEach(function(item){
+              results.usedItems.forEach(function (item) {
                 usedItems.push(item);
               });
-              for(let i=0; i< oldItems.length; i++){
+              for (let i = 0; i < oldItems.length; i++) {
                 used = false;
                 objectUsed = null;
-                for(let j=0; j< usedItems.length; j++){
-                  if(usedItems[j].name === oldItems[i].name){
+                for (let j = 0; j < usedItems.length; j++) {
+                  if (usedItems[j].name === oldItems[i].name) {
                     used = true;
                     indexUsed = j;
                     break;
                   }
                 }
-                if(!used){
+                if (!used) {
                   newItems.push(oldItems[i]);
                 }
               }
-              if(results.rewards.items) {
+              if (results.rewards?.items) {
                 finalItems = [...newItems, ...results.rewards.items];
               } else {
                 finalItems = newItems;
               }
             } else {
-              if(results.rewards.items) {
+              if (results.rewards?.items) {
                 finalItems = [...monk.items, ...results.rewards.items];
               }
-            } 
-            if(results.rewards && (results.mission.type !== 'protect' || 
-              (results.mission.type === 'protect' && results.objectiveHP > 0))){
-              if(results.rewards.spells){
-                // here we check if the monk already has these spells 
+            }
+            if (
+              results.rewards &&
+              (results.mission.type !== "protect" ||
+                (results.mission.type === "protect" && results.objectiveHP > 0))
+            ) {
+              if (results.rewards.spells) {
+                // here we check if the monk already has these spells
                 let newSpells = [];
-                results.rewards.spells.forEach(spell => {
+                results.rewards.spells.forEach((spell) => {
                   let monkHasIt = false;
-                  monk.spells.forEach(monkSpell => {
-                    if( monkSpell.name === spell.name){
+                  monk.spells.forEach((monkSpell) => {
+                    if (monkSpell.name === spell.name) {
                       monkHasIt = true;
                     }
-                  })
-                  if( !monkHasIt ) newSpells.push(spell);
+                  });
+                  if (!monkHasIt) newSpells.push(spell);
                 });
                 finalSpells = [...monk.spells, ...newSpells];
               }
             }
-            setMonk({...monk, spells: [...finalSpells], items: [...finalItems]});
+            setMonk({
+              ...monk,
+              spells: [...finalSpells],
+              items: [...finalItems],
+            });
             setGameScreen({ type: "menu" });
           }}
         />
